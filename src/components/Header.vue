a6a6e2318d078ec225a6cd5ba77fdf1f56a397e8
Mahdi Nasser <81222627+Eecodev@users.noreply.github.com>
2024-06-28 10:50:00 +0200

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+div{
+        height: 20px;
+        background-color: $color-green;
+        border: 2px solid $color-white;
+        border-radius: 20px;
+    }
+</style>
